f122e4393557b9d75b4aa1149dd9943370763c06
Justin Belcher <justin.belcher@gmail.com>
2024-12-31 15:02:26 -0800

    Carousel finalized

--- src/scripts/carousel.ts
+++ src/scripts/carousel.ts
@@ -1,53 +1,31 @@
-export const initCarousel = (
-  element: HTMLElement,
-  currentLabel: HTMLElement,
-  totalItems: number,
-) => {
-  element.addEventListener("click", (event) => {
-    if (element.classList.contains("dragging")) {
-      return;
+export const initCarousel = (el: HTMLElement, label: HTMLElement) => {
+  let isDragging = false;
+  const EDGE_FACTOR = 0.5;
+
+  el.addEventListener("click", (event) => {
+    if (!isDragging) {
+      const elRect = el.getBoundingClientRect();
+      const edgeThreshold = elRect.width * EDGE_FACTOR;
+
+      if (event.clientX - elRect.left < edgeThreshold) {
+        el.scrollBy({
+          left: -el.offsetWidth,
+          behavior: "smooth",
+        });
+      } else if (elRect.right - event.clientX < edgeThreshold) {
+        el.scrollBy({
+          left: el.offsetWidth,
+          behavior: "smooth",
+        });
+      }
     }
-
-    const nextOffset = getNextOffsetOnClick(
-      element.offsetWidth,
-      event.offsetX,
-      element.scrollLeft,
-      totalItems,
-    );
-
-    element.scrollTo({
-      left: nextOffset,
-      behavior: "smooth",
-    });
   });
 
-  element.addEventListener("scrollend", () => {
-    currentLabel.textContent = getIndexAfterScroll(element);
+  el.addEventListener("scrollend", () => {
+    const currentLeftOffset = el.scrollLeft;
+    const elementWidth = el.offsetWidth;
+    const index = Math.round(currentLeftOffset / elementWidth) + 1;
+
+    label.textContent = index.toString().padStart(2, "0");
   });
-};
-
-const getIndexAfterScroll = (el: HTMLElement) => {
-  const currentLeftOffset = el.scrollLeft;
-  const elementWidth = el.offsetWidth;
-  const index = Math.round(currentLeftOffset / elementWidth) + 1;
-
-  return index.toString().padStart(2, "0");
-};
-
-const getNextOffsetOnClick = (
-  elementWidth: number,
-  clickX: number,
-  currOffset: number,
-  totalPhotos: number,
-) => {
-  const index = Math.round(currOffset / elementWidth);
-
-  let targetIndex;
-  if (clickX < elementWidth / 2) {
-    targetIndex = index - 1;
-  } else {
-    targetIndex = index + 1;
-  }
-
-  return Math.max(0, Math.min(targetIndex, totalPhotos - 1)) * elementWidth;
 };
